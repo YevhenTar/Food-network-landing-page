14974c1a365611bf23f7182fecacb99d004e5e33
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-02-22 00:59:24 +0200

    make styles for features-section and about-section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 10) {
+            $('.main-header').addClass('stickytop');
+        } else {
+            $('.main-header').removeClass('stickytop');
+        }
+    });
+});
+
 const swiper = new Swiper('.testimonials-swiper', {
     // Optional parameters
     direction: 'horizontal',
