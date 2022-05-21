62dbe9ddcad3c23934a81a7ab034fe09871c2658
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-05-21 10:27:59 +0300

    Add burger menu

--- js/script.js
+++ js/script.js
@@ -32,6 +32,40 @@ if (window.innerWidth >= 992) {
     });
 }
 
+let menuOpened = false;
+let first = document.querySelector('.top-line');
+let second = document.querySelector('.middle-line');
+let third = document.querySelector('.bottom-line');
+let hiddenMenu = document.querySelector('.hidden-nav__block');
+
+window.addEventListener('click', () => {
+    if (menuOpened == false) {
+        console.log('Opening...');
+        // hiddenMenu.style.display = "block";
+        first.style.marginTop = '10px';
+        second.style.marginTop = '-2px';
+        second.style.transform = 'scale(0)';
+        third.style.marginTop = '-9px';
+        window.setTimeout(() => {
+            first.style.transform = 'rotate(-135deg)';
+            third.style.transform = 'rotate(-225deg)';
+        }, 1000);
+        menuOpened = true;
+    } else if (menuOpened == true) {
+        console.log('Closing...');
+        // hiddenMenu.style.display = "none";
+        first.style.transform = 'rotate(0deg)';
+        third.style.transform = 'rotate(0deg)';
+        window.setTimeout(() => {
+            first.style.marginTop = '0px';
+            second.style.marginTop = '7px';
+            second.style.transform = 'scale(1)';
+            third.style.marginTop = '7px';
+        }, 1000);
+        menuOpened = false;
+    };
+});
+
 const swiper = new Swiper('.testimonials-swiper', {
     // Optional parameters
     direction: 'horizontal',
